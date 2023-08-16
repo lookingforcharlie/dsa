3a8b51113707b04235c2be3e32e89203b21873c3
Charlie Feng <77415402+lookingforcharlie@users.noreply.github.com>
2023-08-16 02:46:50 -0400

    Time: 47 ms (58.78%), Space: 16.4 MB (33.10%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -8,6 +8,8 @@ class Solution:
         for i, char in enumerate(shortest):
             for string in strs:
                 if string[i] != char:
+                    # Return the common prefix up to index i
                     return shortest[:i]
 
+        # If all characters match, return the shortest string
         return shortest
