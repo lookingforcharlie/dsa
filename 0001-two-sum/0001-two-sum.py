ab9b44831c11144250fda7bad8b79b14495f1e22
Charlie Feng <77415402+lookingforcharlie@users.noreply.github.com>
2023-10-19 11:00:56 -0400

    Time: 71 ms (48.21%), Space: 17.5 MB (54.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,14 +1,8 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        num_to_index = {}  
-    
+        num_and_index = {}
         for index, num in enumerate(nums):
-            complement = target - num
-            
-            if complement in num_to_index:
-                return [num_to_index[complement], index]
-            
-            num_to_index[num] = index
-        
-        return None 
-        
+            num_for_search = target - num
+            if num_for_search in num_and_index: 
+                return [index, num_and_index[num_for_search]]
+            num_and_index[num] = index
