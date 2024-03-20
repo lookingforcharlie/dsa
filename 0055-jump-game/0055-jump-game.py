c222ef07826c0dcd9d801ac97cf22c984b965035
Charlie Feng <77415402+lookingforcharlie@users.noreply.github.com>
2024-03-21 07:10:26 +0800

    Time: 354 ms (65.47%), Space: 17.8 MB (57.87%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,10 +1,15 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         index_of_goal  = len(nums) - 1 
+        
+        # if list doesn't have 0, reture True 
         if any(item == 0 for item in nums):
             print("The list contains 0.")
+            
+            # loop through the list from the end to the beginning
             for i in range(index_of_goal - 1, -1, -1):
                 if nums[i] >= index_of_goal - i:
+#                    # if the item can jump to the goal, the index of the item becomes the goal
                     index_of_goal = i
             if index_of_goal == 0: 
                 return True
@@ -15,4 +20,5 @@ class Solution:
         
 
     
-# if list doesn't have 0, reture True 
+
+# Using greedy method
