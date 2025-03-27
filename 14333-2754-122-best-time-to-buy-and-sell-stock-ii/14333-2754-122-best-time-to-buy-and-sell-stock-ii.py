b903579a91a1cabb77b9706d3edec39e2987749e
Charlie Feng <77415402+lookingforcharlie@users.noreply.github.com>
2025-03-27 01:19:16 -0400

    Time: 0 ms, Memory: 18.7 MB - LeetHub

--- 14333-2754-122-best-time-to-buy-and-sell-stock-ii/14333-2754-122-best-time-to-buy-and-sell-stock-ii.py
+++ 14333-2754-122-best-time-to-buy-and-sell-stock-ii/14333-2754-122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,6 +1,8 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         # using greedy solution
+        # no need to loop through the whole list to find the best profit
+        # just adds up the profit between neibors
         profit = 0
         for i in range(len(prices) - 1):
             if prices[i + 1] > prices[i]: 
